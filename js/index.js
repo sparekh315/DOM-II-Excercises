433cde56f9a7540d9aff1b8fb2f4765eccf19101
sparekh315 <saniaparekh315@gmail.com>
2020-06-18 16:59:24 -0500

    added event listeners, and stop propagation to different elements on page

--- js/index.js
+++ js/index.js
@@ -1,2 +1,53 @@
 
 // Your code goes here
+
+const HeaderTextColor = document.querySelector('.main-navigation');
+
+
+HeaderTextColor.addEventListener('click', (e) => {
+        e.target.style.color = 'blue';
+})
+
+const HeaderImgIn = document.querySelector('.header-img');
+HeaderImgIn.addEventListener('mouseover', (e) => {
+  e.target.style.transform = "scale(1.1)";
+
+});
+
+const HeaderImgOut = document.querySelector('.header-img');
+HeaderImgOut.addEventListener('mouseout', (e) => {
+  e.target.style.transform = "scale(1.0)";
+});
+
+const introHeaderColor = document.querySelector('.intro-header');
+introHeaderColor.addEventListener('dblclick', (e) => {
+    e.target.style.backgroundColor = 'pink';
+    e.stopPropagation();
+
+})
+
+const introContentColor = document.querySelector('.intro');
+introContentColor.addEventListener('dblclick', (e) => {
+    e.target.style.backgroundColor = "red";
+})
+
+
+const navClickColor = document.querySelectorAll('.nav a').forEach(item => {
+    item.addEventListener('click', (e) => {
+            e.target.style.color = 'green';
+            e.stopPropagation();
+    })
+})
+
+const navMouseOver = document.querySelectorAll('.nav a').forEach(item => {
+    item.addEventListener('mouseover', (e) => {
+        e.target.style.transform = "scale(1.3)";
+    });
+});
+
+
+const navMouseOut = document.querySelectorAll('.nav a').forEach(item => {
+    item.addEventListener('mouseout', (e) => {
+        e.target.style.transform = "scale(1.0)";
+    });
+});
